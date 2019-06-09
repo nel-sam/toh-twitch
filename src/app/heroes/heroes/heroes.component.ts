095b9874da0f14f585f7eb51cf3935006a252800
Nelson <nelson.rs.07@gmail.com>
2019-06-08 20:02:26 -0700

    Using async pipe now

--- src/app/heroes/heroes/heroes.component.ts
+++ src/app/heroes/heroes/heroes.component.ts
@@ -11,15 +11,11 @@ import { Observable } from 'rxjs';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
+  heroes: Observable<Hero[]> ;
 
   constructor(private heroDataService: HeroDataService) { }
 
   async ngOnInit() {
-    this.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  getHeroes(): Observable<Hero[]> {
-    return this.heroDataService.getHeroes();
+    this.heroes = this.heroDataService.getHeroes();
   }
 }
